3e694bc10f7fc0a0f89e0ed1ec64e2c8461a0b2c
Jan1986-cloud <jan@pvmonteur.nl>
2025-07-11 16:37:24 +0200

    "fix"

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 set -e
 
-# Start Gunicorn, listening on the port provided by the $PORT environment variable.
-# Default to port 8000 if $PORT is not set.
-PORT="${PORT:-8000}"
-exec gunicorn -b 0.0.0.0:$PORT src.main:app
+# Start Gunicorn with the correct application module
+exec gunicorn --bind 0.0.0.0:$PORT --workers 1 --threads 8 --timeout 0 "src.main:create_app()"
