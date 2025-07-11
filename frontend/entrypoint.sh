3e30d62abc5195d48526df3d5a250c9f41cddbb9
Jan1986-cloud <jan@pvmonteur.nl>
2025-07-11 15:19:00 +0200

    "fix"

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 set -e
+
+# Use envsubst to replace ${PORT} in the config file with the value of the PORT env var.
+# The output is piped to a new file that Nginx will use.
+envsubst '${PORT}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+
+# Start Nginx in the foreground
 exec nginx -g 'daemon off;'
